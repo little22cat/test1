add49bd5615e21dadc33fa4e72d468ff181a010c
little22cat <1324078775@qq.com>
2023-10-08 19:49:12 +0800

    添加了程序入口注释

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
     public static void main(String[] args) {
+        //hhhhhhh
         SpringApplication.run(TestApplication.class, args);
     }
 
